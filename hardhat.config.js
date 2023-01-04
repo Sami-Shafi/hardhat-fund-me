158321f3038689bd70be0611cb7d0226fd05884c
Sami-Shafi <freelancersami123@gmail.com>
2023-01-05 01:00:55 +0600

    mock deployment and pricefeed automation

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,28 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+	solidity: {
+		compilers: [{ version: "0.8.8" }],
+	},
+	networks: {
+		goerli: {
+			url: process.env.GOERLI_RPC_URL || "",
+			accounts: [process.env.PRIVATE_KEY],
+			chainId: 5,
+		},
+	},
+	etherscan: {
+		apiKey: process.env.ETHERSCAN_API_KEY,
+	},
+	namedAccounts: {
+		deployer: {
+			default: 0,
+		},
+		user: {
+			default: 1,
+		},
+	},
 };
